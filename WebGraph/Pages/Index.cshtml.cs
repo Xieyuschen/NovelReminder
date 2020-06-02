535f8256317b974e06af2ae9af8c2295e0de92ae
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-06-02 13:52:22 +0800

    Show booked novel details in index page

--- WebGraph/Pages/Index.cshtml.cs
+++ WebGraph/Pages/Index.cshtml.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-
+using WebGraph.Models;
+using System.Text.Json;
 namespace WebGraph.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+        public List<Record> Records;
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            Records = new List<Record>();
+            FileStream aFile = new FileStream("wwwroot/DataFile.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            StreamReader reader = new StreamReader(aFile);
+            while (!reader.EndOfStream)
+            {
+                var str = reader.ReadLine();
+                Records.Add(JsonSerializer.Deserialize<Record>(str));
+            }
         }
 
         public void OnGet()
