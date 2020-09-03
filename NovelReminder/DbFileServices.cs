ecbbe2509064c1c8e46c786f3993eee0bce40298
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-09-03 14:24:59 +0800

    finish DbFileService and fix encoutering bugs

--- NovelReminder/DbFileServices.cs
+++ NovelReminder/DbFileServices.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Linq;
+using System.IO;
 namespace NovelReminder
 {
 
+    //This class stores information about a piece of Novel to remind
     public class NovelInfo
     {
-        string Url { get; set; }
-        int LastUpdate {get;set;}
-        bool IsInit {get;set;}
+        public string Url { get; set; }
+        public int LastUpdate { get; set;}
+        public bool IsInit {get;set;}
+
+        private static string split =  ";";
+        public string Serialization()
+        {
+            return Url + split + LastUpdate.ToString() + IsInit.ToString();
+        }
+        public static NovelInfo Deserialization(string str)
+        {
+            var info = str.Split(split);
+            NovelInfo novelinfo = new NovelInfo{
+                Url = info[0],
+                LastUpdate = int.Parse(info[1]),
+                IsInit = bool.Parse(info[2])
+            };
+            return novelinfo;
+        }
     }
+    
     /// <summary>
     /// Manage updating information by a txt file rather a database
     /// </summary>
@@ -23,33 +42,40 @@ namespace NovelReminder
         {
             //rewrite txt file when some records are updated
         }
-        public DbfileServices()
+        public DbFileServices()
         {
             //Init infomation by file NovelInfomation.txt
-            //do logical steps
-        }
-        public ValueTask<bool> GetIsInit(string url)
-        {
-            var IsInit=from s in Infomation
-                       where s.Url==url
-                       select s.IsInit;
-            if(IsNullReference(IsInit))
+            //Txt or Json?
+            //Json can simplied Serialization!
+
+            //how to Serilize NovelInfo and Deserilize it?
+            //Implement it by myself first:)
+            string filename = @"E:\Code\NovelReminder\NovelReminder\NovelInfomation.txt";
+            IEnumerable<string> lines = File.ReadLines(filename);
+            foreach(var item in lines)
             {
-                throw new Exception();
+                Information.Add(NovelInfo.Deserialization(item));
             }
+        }
+
+        //Lack of async will occurs that cannot convert bool to System.Threading.Task
+        public async ValueTask<bool> GetIsInit(string url)
+        {
+            var IsInit = Information.Find(t => t.Url == url).IsInit;
+            
             return IsInit;
         }
 
-        public ValueTask<int> GetLastChapterAsync(string url)
+        public async ValueTask<int> GetLastChapterAsync(string url)
         {
             var lastchapter= from item in Information
                             where item.Url==url
-                            select item.LastChapter;
+                            select item.LastUpdate;
                          
-            return lastchapter;
+            return lastchapter.First();
         }
 
-        public ValueTask InsertOrUpdateOneAsync(string url, int lastUpdate)
+        public async ValueTask InsertOrUpdateOneAsync(string url, int lastUpdate)
         {
             bool IsUpdate=false;
             foreach(var item in Information)
@@ -57,19 +83,20 @@ namespace NovelReminder
                 if(item.Url==url)
                 {
                     IsUpdate=true;
-                    item.LastChapter=lastUpdate;
+                    item.LastUpdate=lastUpdate;
                 }
 
             }
             if(!IsUpdate)
             {
                 Information.Add(new NovelInfo{
-                    Url=url,LastChapter=lastUpdate,IsInit=false}
-                    );
+                    Url=url,
+                    LastUpdate=lastUpdate
+                    ,IsInit=false});
             }
         }
 
-        public ValueTask UpdateAsync(string url, bool isinit)
+        public async ValueTask UpdateAsync(string url, bool isinit)
         {
             foreach(var item in Information)
             {
@@ -80,7 +107,7 @@ namespace NovelReminder
             }
         }
 
-        public ValueTask UpdateAsync(string url, int lastUpdate)
+        public async ValueTask UpdateAsync(string url, int lastUpdate)
         {
             foreach(var item in Information)
             {
--- NovelReminder/Program.cs
+++ NovelReminder/Program.cs
@@ -18,21 +18,23 @@ namespace Try
     {
         static async System.Threading.Tasks.Task Main(string[] args)
         {
-            string jsonfile = @"C:\Users\DELL\AppData\Roaming\Microsoft\UserSecrets\ee6f7777-9738-4ddc-b287-7868412d3df1\secrets.json";
-            StreamReader file = File.OpenText(jsonfile);
-            JsonTextReader reader = new JsonTextReader(file);
-            JObject o = (JObject)JToken.ReadFrom(reader);
-            var emailToken = o["password"].ToString();
-            var option = new SmtpClientOptions
-            {
-                Account = "1743432766@qq.com",
-                Token = emailToken
-            };
-            var em = new EmailService(option);
-            Reminder reminder = new Reminder(new DatabaseService(),em,new Scanner());
-            reminder.AddBooksUrl("http://www.biquge.se/12809/");
-            reminder.AddReceiver("2016231075@qq.com");
-            await reminder.StartAsync();
+            //string jsonfile = @"C:\Users\DELL\AppData\Roaming\Microsoft\UserSecrets\ee6f7777-9738-4ddc-b287-7868412d3df1\secrets.json";
+            //StreamReader file = File.OpenText(jsonfile);
+            //JsonTextReader reader = new JsonTextReader(file);
+            //JObject o = (JObject)JToken.ReadFrom(reader);
+            //var emailToken = o["password"].ToString();
+            //var option = new SmtpClientOptions
+            //{
+            //    Account = "1743432766@qq.com",
+            //    Token = emailToken
+            //};
+            //var em = new EmailService(option);
+            //Reminder reminder = new Reminder(new DatabaseService(),em,new Scanner());
+            //reminder.AddBooksUrl("http://www.biquge.se/12809/");
+            //reminder.AddReceiver("2016231075@qq.com");
+            //await reminder.StartAsync();
+
+
         }
     }
 }
