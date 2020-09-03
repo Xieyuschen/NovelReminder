2c3e2e1d64732508e4d1ae166c1006401e1f9598
Xieyuschen <52945328+Xieyuschen@users.noreply.github.com>
2020-09-03 11:41:52 +0800

    Implement a DbFileService

--- NovelReminder/DbFileServices.cs
+++ NovelReminder/DbFileServices.cs
@@ -2,43 +2,93 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Linq;
 namespace NovelReminder
 {
 
     public class NovelInfo
     {
         string Url { get; set; }
+        int LastUpdate {get;set;}
+        bool IsInit {get;set;}
     }
     /// <summary>
-    /// Manage updating information by a txt file rather database
+    /// Manage updating information by a txt file rather a database
     /// </summary>
     class DbFileServices : IDataManager
     {
-
+        private List<NovelInfo> Information {get;set;}
+        
+        private void RewriteTxtFile()
+        {
+            //rewrite txt file when some records are updated
+        }
+        public DbfileServices()
+        {
+            //Init infomation by file NovelInfomation.txt
+            //do logical steps
+        }
         public ValueTask<bool> GetIsInit(string url)
         {
-            throw new NotImplementedException();
+            var IsInit=from s in Infomation
+                       where s.Url==url
+                       select s.IsInit;
+            if(IsNullReference(IsInit))
+            {
+                throw new Exception();
+            }
+            return IsInit;
         }
 
         public ValueTask<int> GetLastChapterAsync(string url)
         {
-            throw new NotImplementedException();
+            var lastchapter= from item in Information
+                            where item.Url==url
+                            select item.LastChapter;
+                         
+            return lastchapter;
         }
 
         public ValueTask InsertOrUpdateOneAsync(string url, int lastUpdate)
         {
-            throw new NotImplementedException();
+            bool IsUpdate=false;
+            foreach(var item in Information)
+            {
+                if(item.Url==url)
+                {
+                    IsUpdate=true;
+                    item.LastChapter=lastUpdate;
+                }
+
+            }
+            if(!IsUpdate)
+            {
+                Information.Add(new NovelInfo{
+                    Url=url,LastChapter=lastUpdate,IsInit=false}
+                    );
+            }
         }
 
         public ValueTask UpdateAsync(string url, bool isinit)
         {
-            throw new NotImplementedException();
+            foreach(var item in Information)
+            {
+                if(item.Url==url)
+                {
+                    item.IsInit=isinit;
+                }
+            }
         }
 
         public ValueTask UpdateAsync(string url, int lastUpdate)
         {
-            throw new NotImplementedException();
+            foreach(var item in Information)
+            {
+                if(item.Url==url)
+                {
+                    item.LastUpdate=lastUpdate;
+                }
+            }
         }
     }
 }
